b564af698ba51392bff3c2d8b2e090d04b9bb2b7
sudarmanis <41697795+sudarmanis@users.noreply.github.com>
2019-08-24 07:30:22 -0700

    Create co.java

---
+++ co.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class co
+{
+public static void main(String argS[])
+{
+int a,m=0;
+Scanner sc=new Scanner(System.in);
+System.out.println("enter the no");
+a=sc.nextInt();
+while(a!=0)
+{
+	a=a/10;
+	m++;
+	
+}
+System.out.println(m);
+	}
+}
