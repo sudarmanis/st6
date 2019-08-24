1c127276f1af274744d5670922ddf0747d4a6180
sudarmanis <41697795+sudarmanis@users.noreply.github.com>
2019-08-24 07:23:14 -0700

    Create sev.java

---
+++ sev.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class sev
+{
+public static void main(String argS[])
+{
+int temp;
+Scanner sc=new Scanner(System.in);
+System.out.println("enter the initial no");
+temp=sc.nextInt();
+for(int i=temp;i<=temp;i++)
+{
+ if(i%2==0)
+ {
+ System.out.println(i);
+ }
+else
+{
+System.out.println(i-1);	
+	
+ }
+ }
+ 
+}}
