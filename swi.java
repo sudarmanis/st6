1b4eaae1342ac320c520cd18de3cb197f04acd72
sudarmanis <41697795+sudarmanis@users.noreply.github.com>
2019-08-24 07:02:12 -0700

    Create swi.java

---
+++ swi.java
@@ -0,0 +1,34 @@
+import java.util.*;
+class swi
+{
+public static void main(String argS[])
+{
+int c ;
+Scanner sc=new Scanner(System.in);
+
+System.out.println("enter the no");
+c=sc.nextInt();
+switch(c)
+{
+case 1:System.out.println("one");
+break;
+case 2:System.out.println("two");
+break;
+case 3:System.out.println("three");
+break;
+case 4:System.out.println("four");
+break;
+case 5:System.out.println("five");
+break;
+case 6:System.out.println("six");
+break;
+case 7:System.out.println("seven");
+break;
+case 8:System.out.println("eight");
+break;
+case 9:System.out.println("nine");
+break;
+case 10:System.out.println("ten");
+break;
+}
+}}
