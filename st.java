5c70640e2855804a8687e417af3e151263d460b8
sudarmanis <41697795+sudarmanis@users.noreply.github.com>
2019-08-24 07:36:17 -0700

    Create st.java

---
+++ st.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class st
+{
+public static void main(String argS[])
+{
+int a,m,n=0;
+Scanner sc=new Scanner(System.in);
+System.out.println("enter the no");
+a=sc.nextInt();
+for(m=1;m<=a;m++)
+{
+n=n+m;
+	}
+	System.out.println(n)
+	}
+}
